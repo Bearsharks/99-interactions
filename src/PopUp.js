c1d79f641d61b6224f1a8193d6fa583fe999772e
bearsharks <bearsharks02@gmail.com>
2021-06-14 18:26:46 +0900

    팝업사진이 웹서치결과링크로 제공되도록 수정함

--- src/PopUp.js
+++ src/PopUp.js
@@ -1,16 +1,17 @@
-import React, { useEffect,useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './PopUp.module.css';
-function PopUp(props){
+function PopUp(props) {
     let abs;
-    if(props.imageInfo !== null){
-        abs = { 
+    if (props.imageInfo !== null) {
+        abs = {
             position: 'absolute',
             left: `${props.imageInfo.popUpPos[0]}px`,
             top: `${props.imageInfo.popUpPos[1]}px`,
-            width : `${props.imageInfo.popUpSize}px`,
-            height : `${props.imageInfo.popUpSize}px`,
+            width: `${props.imageInfo.popUpSize}px`,
+            height: `${props.imageInfo.popUpSize}px`,
         };
     }
+    debugger;
     /*{
       popUpSize : popUpSize,
       popUpPos : popUpPos,
@@ -22,11 +23,23 @@ function PopUp(props){
     }*/
     return (
         <>
-        {props.imageInfo &&
-            <div className={styles.line} style={abs} onMouseLeave={props.hide}>            
-                <img className={styles.width100} src={props.imageInfo.thumbnailUrl+`&w=${props.imageInfo.popUpSize}&h=${props.imageInfo.popUpSize}&c=7`}/>            
-            </div>
-        }
+            {props.imageInfo &&
+                <div className={styles.line} style={abs} onMouseLeave={props.hide}>
+                    <a
+                        href={props.imageInfo.webSearchUrl}
+                        target='_blank'
+                        rel="noreferrer"
+                    >
+                        <img
+                            className={styles.width100}
+                            src={props.imageInfo.thumbnailUrl + `&w=${props.imageInfo.popUpSize}&h=${props.imageInfo.popUpSize}&c=7`}
+                            alt={props.imageInfo.name}
+                        />
+                    </a>
+
+
+                </div>
+            }
         </>
     );
 }
